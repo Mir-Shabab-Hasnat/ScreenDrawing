a3455d2da39e0057adb9ff7cea79409498040e3c
Mir Shabab Hasnat <shababhasnat@gmail.com>
2024-05-09 22:13:41 -0600

    added fps

--- HandTracking.py
+++ HandTracking.py
@@ -8,6 +8,9 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+prevTime = 0
+currTime = 0
+
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -16,8 +19,19 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
+    currTime = time.time()
+    fps = 1/(currTime-prevTime)
+    prevTime = currTime
+
+    cv2.putText(img,
+                str(int(fps)),
+                (10, 70),
+                cv2.FONT_HERSHEY_PLAIN,
+                3,
+                (255, 0, 255),
+                3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
