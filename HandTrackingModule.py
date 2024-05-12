9a04b32362fa2e6358def4b31519a02a58c36295
Mir Shabab Hasnat <shababhasnat@gmail.com>
2024-05-12 12:17:58 -0600

    hand detector initialised

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -2,17 +2,24 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(0)
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+class HandDetector():
+    def __init__(self, mode=False, maxHands=2, detectionConf=0.5, trackConf=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionConf = detectionConf
+        self.trackConf = trackConf
 
-prevTime = 0
-currTime = 0
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode,
+                                        max_num_hands=self.maxHands,
+                                        min_detection_confidence=self.detectionConf,
+                                        min_tracking_confidence=self.trackConf)
+        self.mpDraw = mp.solutions.drawing_utils
 
-while True:
-    success, img = cap.read()
+
+
+
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     #print(results.multi_hand_landmarks)
@@ -20,27 +27,38 @@ while True:
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
-
                 height, width, channels = img.shape
                 channelx, channely = int(lm.x * width), int(lm.y * height)
                 print(id, channelx, channely)
 
                 cv2.circle(img, (channelx, channely), 25, (255, 0, 255), cv2.FILLED)
 
-
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
-    currTime = time.time()
-    fps = 1/(currTime-prevTime)
-    prevTime = currTime
 
-    cv2.putText(img,
-                str(int(fps)),
-                (10, 70),
-                cv2.FONT_HERSHEY_PLAIN,
-                3,
-                (255, 0, 255),
-                3)
+def main():
+    cap = cv2.VideoCapture(0)
+    prevTime = 0
+    currTime = 0
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    while True:
+        success, img = cap.read()
+
+        currTime = time.time()
+        fps = 1 / (currTime - prevTime)
+        prevTime = currTime
+
+        cv2.putText(img,
+                    str(int(fps)),
+                    (10, 70),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    3,
+                    (255, 0, 255),
+                    3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
