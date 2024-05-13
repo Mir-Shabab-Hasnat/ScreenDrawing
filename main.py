df218ad03807eaccab95daa7ad05c9c41dad1600
Mir Shabab Hasnat <shababhasnat@gmail.com>
2024-05-12 19:49:11 -0600

    setting the heaDER image in the vidcap

--- main.py
+++ main.py
@@ -6,11 +6,22 @@ import HandTrackingModule as htm
 
 folderPath = "HeaderImages"
 myList = os.listdir(folderPath)
-print(myList)
 
 overlayList = []
 for imgPath in myList:
     image = cv2.imread(f"{folderPath}/{imgPath}")
     overlayList.append(image)
 
-print(len(overlayList))
+header = overlayList[0]
+
+vidCap = cv2.VideoCapture(0)
+vidCap.set(3, 1920)
+vidCap.set(4, 720)
+
+while True:
+    success, img = vidCap.read()
+
+    # Setting the header image
+    img[0:200, 0:1920] = header
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
