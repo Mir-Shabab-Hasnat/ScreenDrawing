f24369598d7cd9d9dab8e16f12cbe808beea7e4f
Mir Shabab Hasnat <shababhasnat@gmail.com>
2024-05-14 23:56:23 -0600

    editted the screen and images

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ while True:
 
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
+    print(lmList[8][1])
 
     if len(lmList) != 0:
         topOfIndexFingerX, topOfIndexFingerY = lmList[8][1], lmList[8][2]
@@ -37,14 +38,25 @@ while True:
         # Check which fingers are up
 
         fingers = detector.fingersUp()
-        print(fingers)
 
         # If Selection Mode - Two fingers up
         if fingers[1] and fingers[2]:
-            print("Selection Mode")
+            cv2.rectangle(img, (topOfIndexFingerX, topOfIndexFingerY - 25),
+                          (topOfMiddleFingerX, topOfMiddleFingerY + 25),
+                          (255, 0, 255), cv2.FILLED)
+            #print("Selection Mode")
+
+            # Checking for click
+            if topOfIndexFingerY < 100:
+                if 250 < topOfIndexFingerX < 450:
+                    header = overlayList[1]
+                elif 550 < topOfIndexFingerX < 750:
+                    header = overlayList[2]
 
         if fingers[1] and fingers[2] == False:
-            print("Drawing Mode")
+            cv2.circle(img, (topOfIndexFingerX, topOfIndexFingerY),
+                       15, (255, 0, 255), cv2.FILLED)
+            #print("Drawing Mode")
 
     # If Drawing Mode - Index Finger up
 
